d9f82e49d38880763ab6b27dff7b9af5bba87e46
穿越的杨宗宝 <15732633043@163.com>
2025-11-04 16:17:22 +0800

    fix:删除build目录

--- build/CMakeFiles/app.dir/editor/ipc/editor-ipc.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/app.dir/editor/ipc/editor-ipc.cpp.o: \
-  /Users/yangzongyuan/personal/project/Boo-Engine/editor/ipc/editor-ipc.cpp
--- build/CMakeFiles/app.dir/engine/core/math/quat.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/app.dir/engine/core/math/quat.cpp.o: \
-  /Users/yangzongyuan/personal/project/Boo-Engine/engine/core/math/quat.cpp \
-  /Users/yangzongyuan/personal/project/Boo-Engine/engine/core/math/quat.h
